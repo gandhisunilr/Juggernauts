cf038ada0d684f6f3bceceeeb0f45c50767f7f40
Sunil <sunil.gandhi007@gmail.com>
2012-02-16 15:01:10 +0530

    Changes in main.c

--- main.c
+++ main.c
@@ -1,37 +1,54 @@
 #include<stdint.h>
-__attribute__((section("sumfunstart")))
-//sumfunstart=.;
-int sum()
+
+int a;
+//__attribute__ ((section (".flashfun")))
+__attribute__ ((section (".sumfun")))
+void sum() 
 {
-return 2+1;
-}
-//sumfunend=.;
-__attribute__((section("sumfunend")))
-int main(void)
-{
-	extern unsigned sumfunstart,sumfunend;
-	int *codeaddr,*src,*dest;
+
+	a=a+1;
 	
-	int (*fp)(void);
-	codeaddr=(0x20000097|0x01);
-		for(dest=codeaddr,
-			src=&sumfunstart; src<=&sumfunend;
-				src++,dest++)
-		{	
-			*dest=*src;
-		}	
-        
-//	a=a/i;
-        fp=codeaddr;
-//	goto *codeaddr;
-	fp();	
-	while(1);
-}
-int sub()
-{
-return 2-1;
-}
-void usage_fault_handler(){
-return;
+	return;
 }
 
+
+//__attribute__ ((section (".ramfun")))
+__attribute__ ((section (".subfun")))
+void sub()
+{
+	a=a-1;
+	return;
+}
+
+extern unsigned ramfunstart, main_end, main_start, myfun_start, myfun_end;
+__attribute__ ((section (".ramfun")))
+void ramfun()
+{
+		volatile unsigned *src,*dest;
+
+	for(dest= (&ramfunstart - &myfun_start + &myfun_end), src= &main_start; src<= &main_end; src++, dest++){
+			*dest=*src;
+		}
+
+	return;
+}
+
+
+__attribute__((section("mainfun")))
+//__attribute__((section(".XNregion")))
+int main(void)
+{
+	
+	a=10;
+	
+	a=a+1;
+}
+
+
+void usage_fault_handler(){
+
+	
+
+	
+	return;
+}
